c87d212eb988080602c3accf0a6502c46096aa1f
Brook Luers <brook@Brooks-MacBook-Air.local>
2020-03-30 12:29:10 -0400

    text labels, better scales

--- app.R
+++ app.R
@@ -22,23 +22,30 @@ thm <- theme(panel.background = element_rect(color='grey', fill=NA),
              legend.text = element_text(size=11,color='black'),
              legend.key = element_rect(fill=NA))
 logBreaks <- function(x) {
-  if (x[2] < 10){
-    return(seq(trunc(x[1]), round(x[2],2),length.out=4))
-  } else{
-    return(seq(trunc(x[1]), trunc(x[2])+1)) 
-  }
+  xs <- seq(trunc(x[1]), x[2], length.out=8)
+  xs10 <- 10^xs
+  return(unique(log10(ifelse(xs10 < 1, signif(xs10,1),round(xs10)))))
 }
 logLabels <- function(x) {
   if (max(x, na.rm=T) < 1){
-    return(formatC(10^x, digits=3, format='f'))
+    return(formatC(10^x, digits=2, format='f'))
   } else {
     return(c(prettyNum(
       ifelse(x < 1, 10^x, round(10^x)), digits=3, big.mark=','))) 
   }
 }
-rawBreaks <- function(x) return(seq(0, x[2] %/% (10^trunc(log10(x[2]))) * (10^trunc(log10(x[2]))),
-                                    by=10^(trunc(log10(x[2])))/2))
-rawLabels <- function(x) return(prettyNum(x,big.mark=','))
+rawBreaks <- function(x){
+  nz <- trunc(log10(x[2]))
+  maxSeq <- x[2] %/% (10^nz) * (10^nz)
+  incr <- 10^ nz / 2
+  ret <- c(seq(0, maxSeq, by=incr),
+           signif(x[2], trunc(log10(x[2]))))
+  if (length(ret) > 8) {
+    ret <- ret[unique(seq(1,length(ret),2),length(ret))]
+  }
+  return(ret)
+} 
+rawLabels <- function(x) return(prettyNum(x, big.mark=','))
 
 
 # Define UI for application that draws a histogram
@@ -46,7 +53,7 @@ ui <- # Use a fluid Bootstrap layout
   fluidPage(    
     
     # Give the page a title
-    titlePanel("Covid-19 Cases by County"),
+    #titlePanel("Covid-19 Cases by County"),
     
     # Generate a row with a sidebar
     sidebarLayout(      
@@ -54,7 +61,8 @@ ui <- # Use a fluid Bootstrap layout
       # Define the sidebar with one input
       sidebarPanel(
         selectInput("response", "Plot:", 
-                    choices = respOptions),
+                    choices = respOptions,
+                    selected='Cases per 100,000 people'),
         
         hr(),
         radioButtons("yscale", "Scale",
@@ -62,8 +70,10 @@ ui <- # Use a fluid Bootstrap layout
         hr(),
         dateRangeInput('dateRange',
                        label = 'Date range:',
-                       start = min(dc$date) , 
-                       end = max(dc$date)
+                       start = Sys.Date() - 30,
+                       min = min(dc$date) , 
+                       end = max(dc$date),
+                       max = max(dc$date)
         ),
         hr(),
         selectizeInput("state", "State:", 
@@ -77,9 +87,7 @@ ui <- # Use a fluid Bootstrap layout
                 target="_blank"),
                 "based on reports from state and local health agencies.",
                 "County and state populations from the U.S. Census Bureau, American Community Survey.",
-                "Visualized by ",
-                a("Brook Luers", href="http://www.brookluers.com/", target="_blank"),
-                " using ",
+                "Visualized with",
                 a("Shiny.", href="https://shiny.rstudio.com/", target="_blank"))
       ),
       
@@ -95,61 +103,92 @@ ui <- # Use a fluid Bootstrap layout
 server <- function(input, output) {
   # Fill in the spot we created for a plot
   output$thePlot <- renderPlot({
+    ndays <- input$dateRange[2] - input$dateRange[1]
     xscale <- scale_x_date(date_labels = "%b %d",
                            date_minor_breaks="1 day",
-                           limits=c(input$dateRange[1], input$dateRange[2]))
+                           breaks = seq(input$dateRange[1], input$dateRange[2],
+                                        length.out=ifelse(ndays < 3, 2,
+                                                          ifelse(ndays < 8, 3, 5))),
+                           limits= c(input$dateRange[1], input$dateRange[2] +
+                                       ifelse(ndays < 5, 1,
+                                              ifelse(ndays < 9, 2,
+                                                     ifelse(ndays < 20, 3, 5)))))
+    nstates <- length(input$state)
+    fadd <- rep(c(1,1.5,2),each=3)
+    xscale_facet <- scale_x_date(date_labels = "%b %d",
+                                           date_minor_breaks="2 days",
+                                           breaks = seq(input$dateRange[1], input$dateRange[2],
+                                                        length.out=ifelse(ndays < 3, 2, 3)),
+                                           limits= c(input$dateRange[1], input$dateRange[2] +
+                                                       ifelse(ndays < 5, 1,
+                                                              ifelse(ndays < 9, 4,
+                                                                     ifelse(ndays < 20, 5, 6)))*fadd[nstates]))
     stateColScale <- scale_color_manual(values = 
                                           c(setNames(brewer.pal(maxNStates, 
                                                        'Set1'), 
-                                                     c(input$state,rep('',maxNStates-length(input$state)))),
+                                                     c(input$state,rep('',maxNStates-nstates))),
                                                  'other'='grey'),
                                         breaks=input$state,
                                         name='') 
     plds <- ds %>%
       filter(date >= input$dateRange[1] & date <= input$dateRange[2]) %>%
       mutate(stateCut = ifelse(state %in% input$state, state, 'other'))
+    pldsText <-
+      plds %>% group_by(state) %>% filter(date==max(date)) %>% ungroup
     pldc <- dc %>%
       filter(date >= input$dateRange[1] & date <= input$dateRange[2])
     if (!is.null(input$state)){
       pldc <- filter(pldc, state %in% input$state)
     } 
+    pldcText <-
+      pldc %>% group_by(countystate) %>% filter(date==max(date)) %>% ungroup
     if (input$yscale == 'Logarithmic'){
-      ps <- 
-        plds %>%
-        ggplot(aes_string('date', 
-                          lrespVarMap[input$response],
-                          group = 'state')) + 
-        scale_y_continuous(breaks=logBreaks, labels=logLabels)
-      pc <- pldc %>%
-        ggplot(aes_string('date', 
-                          lrespVarMap[input$response], 
-                          group = 'countystate')) +
-        scale_y_continuous(breaks=logBreaks, labels=logLabels)
+      respvar <- lrespVarMap[input$response]
+      yscale <- scale_y_continuous(breaks=logBreaks, labels=logLabels)
     } else {
-      ps <- 
-        plds %>%
-        ggplot(aes_string('date',
-                          respVarMap[input$response],
-                          group = 'state')) +
-        scale_y_continuous(breaks=rawBreaks, labels=rawLabels)
-      pc <- 
-        pldc %>%
-        ggplot(aes_string('date', respVarMap[input$response], 
-                          group = 'countystate')) + 
-        scale_y_continuous(breaks=rawBreaks,
-                           labels=rawLabels)
+      respvar <- respVarMap[input$response]
+      yscale <- scale_y_continuous(breaks=rawBreaks, labels=rawLabels)
     } 
+    ps <- 
+      plds %>%
+      ggplot(aes_string('date', respvar,
+                        group = 'state')) + yscale
+    pc <- 
+      pldc %>%
+      ggplot(aes_string('date', respvar, 
+                        group = 'countystate')) + yscale
     if (is.null(input$state)) {
-      pc <- pc + geom_line()
-      ps <- ps + geom_line()
+      pc <- pc + geom_line() +
+        geom_text(aes_string('date', respvar,
+                             label='lab'),
+                  hjust=0,vjust=0.5,size=3,
+                  check_overlap = TRUE,
+                  data=pldcText %>% top_n(2, !!sym(respvar)) %>%
+                    mutate(lab = str_replace(as.character(countystate), '\\.',',\n')))
+      ps <- ps + geom_line() +
+        geom_text(aes_string('date',respvar,
+                             label='state'),
+                  check_overlap = TRUE,
+                  hjust=0,vjust=0,size=3.5,
+                  data=top_n(pldsText, 2, !!sym(respvar)))
     } else {
       pc <- pc +
         geom_line(aes(color=state)) +
+        geom_text(aes_string('date',respvar,
+                             label='county'),
+                  hjust=0,vjust=0,size=3.1,
+                  check_overlap = TRUE,
+                  data=pldcText %>% group_by(state) %>% top_n(1, !!sym(respvar))) +
         stateColScale + 
         guides(list(color='none'))
       ps <- ps +
         geom_line(aes(color = stateCut, alpha=(stateCut=='other'),
                       size = (stateCut == 'other'))) +
+        geom_text(aes_string('date',respvar,
+                             label='state', color='stateCut'),
+                  hjust=0,vjust=0,size=3.5,check_overlap = TRUE,
+                  data = filter(pldsText, state %in% input$state) %>%
+                    top_n(2, !!sym(respvar))) +
         stateColScale + scale_alpha_manual(breaks=c('TRUE','FALSE'),
                                             values=c(1, 1/8)) +
         scale_size_manual(breaks=c('TRUE','FALSE'),
@@ -161,9 +200,11 @@ server <- function(input, output) {
     pc <- pc +
       ggtitle(ifelse(is.null(input$state), "All US Counties", 
                      "Counties in...")) +
-      thm + xscale + xlab("") + ylab(paste(respLabels[input$response], ' ',sep='')) 
+      thm + xlab("") + ylab(paste(respLabels[input$response], ' ',sep='')) 
     if (!is.null(input$state)) {
-      pc <- pc + facet_wrap(~state, scales='fixed', nrow=3)
+      pc <- pc + facet_wrap(~state, scales='fixed', nrow=3) + xscale_facet
+    } else { 
+      pc <- pc + xscale
     }
     ps <- ps + ggtitle("States") + thm + xscale + xlab("") +
       ylab(paste(respLabels[input$response], ' ', sep=''))
